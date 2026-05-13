2053c75bcd1b808e75c67677789abf065ddf8a74
CTimanaCasaRJ <cbeteta@peq.coppe.ufrj.br>
2026-05-13 10:02:16 -0300

    Desafio1: Inputs concluidos

---
+++ Desafio1/calculadora_viagem.py
@@ -0,0 +1,9 @@
+print('Calculadora de viagem! Será que a viagem é viável??')
+print('---------------------------------------------------')
+print('\nIMPORTANTE: Usar ponto como separador decimal. Não usar separador de milhares')
+presupuesto = input("\nIndique o orcamento disponivel, em Reais: ")
+destino = input('Indique o nome da cidade ou pais do destino: ')
+pasaje = input('Indique o custo da passagem, em Reais: ')
+hosp = input('Indique o custo diário de hospedagem, em Euros: ')
+dias = input('Indique os días de viagem: ')
+
